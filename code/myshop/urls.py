b18837d3f586b3289126e1082d8c8d20c95c97e8
carloscg00 <carcangom@alum.us.es>
2022-11-30 18:39:28 +0100

    Cambios menores en css e imagenes anyadidas

--- code/myshop/urls.py
+++ code/myshop/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from shop import views
 
 
 urlpatterns = [
+    path('', views.inicio_view),
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('shop/', include('shop.urls', namespace='shop')),
