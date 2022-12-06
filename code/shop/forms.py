4202d1934a6a66a5be78446530b60f8d6a4f238b
carloscg00 <carcangom@alum.us.es>
2022-12-06 19:18:38 +0100

    Ajustes menores

--- code/shop/forms.py
+++ code/shop/forms.py
@@ -2,12 +2,7 @@ from django import forms
 from shop import models
 
 class clienteForm(forms.Form):
-    sugerencia = forms.CharField(required=True)
-
-class reservaForm(forms.Form):
-      servicio =  forms.ChoiceField(choices=[('corte_caballero','Corte para caballero'),
-          ('corte_niño','Corte para niño'),('arreglo_barba','Barba'),('tinte','Tinte')])  
-      informacion_extra = forms.CharField()
+    content = forms.CharField(required=True)
 
 class buscador_productos(forms.Form):
     producto = forms.CharField(required=False)
